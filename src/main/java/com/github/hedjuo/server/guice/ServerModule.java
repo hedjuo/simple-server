3c411777bff9b7d7c657654f90616d619dbe8cbd
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-12-04 03:33:49 +0700

    Delete ServiceManager and ConnectionHandler bindings.

--- src/main/java/com/github/hedjuo/server/guice/ServerModule.java
+++ src/main/java/com/github/hedjuo/server/guice/ServerModule.java
@@ -1,9 +1,6 @@
 package com.github.hedjuo.server.guice;
 
-import com.github.hedjuo.server.ConnectionHandler;
-import com.github.hedjuo.server.PropertyFileServiceLoader;
-import com.github.hedjuo.server.ServiceLoader;
-import com.github.hedjuo.server.ServiceManager;
+import com.github.hedjuo.server.*;
 import com.github.hedjuo.server.services.AuthService;
 import com.github.hedjuo.server.services.DateService;
 import com.github.hedjuo.server.services.UUIDService;
@@ -14,11 +11,11 @@ public class ServerModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(ServiceLoader.class).to(PropertyFileServiceLoader.class).in(Singleton.class);
-        bind(ServiceManager.class).in(Singleton.class);
-        bind(ConnectionHandler.class).in(Singleton.class);
 
         bind(AuthService.class).in(Singleton.class);
         bind(DateService.class).in(Singleton.class);
+
+        bind(ServiceRunner.class).to(MultiThreadServiceRunner.class).in(Singleton.class);
 
         // Internal services
         bind(UUIDService.class).in(Singleton.class);
