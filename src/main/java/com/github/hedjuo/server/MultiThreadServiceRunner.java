ee33939dcc73db2efb2f725a9d224fda51af0dcb
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-12-03 20:53:14 +0700

    Add MultiThreadServiceRunner.

---
+++ src/main/java/com/github/hedjuo/server/MultiThreadServiceRunner.java
@@ -0,0 +1,16 @@
+package com.github.hedjuo.server;
+
+import java.util.concurrent.*;
+
+public class MultiThreadServiceRunner  implements ServiceRunner {
+    private final ExecutorService executorService = new ThreadPoolExecutor(
+            0,
+            Integer.MAX_VALUE,
+            10L,
+            TimeUnit.SECONDS,
+            new SynchronousQueue<>());
+
+    public synchronized Future<Object> executeService(RunServiceActionTask task) {
+        return executorService.submit(task);
+    }
+}
