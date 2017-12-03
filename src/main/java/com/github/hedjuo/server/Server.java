16ea2629b5f320907112d00433c2e80c75bba410
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-12-03 20:57:31 +0700

    Replace usage ConnectionManager with ClientHandlerTask.

--- src/main/java/com/github/hedjuo/server/Server.java
+++ src/main/java/com/github/hedjuo/server/Server.java
@@ -8,13 +8,11 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
     public static Injector INJECTOR = Guice.createInjector(new ServerModule());
 
-    private ConnectionHandler connectionHandler = INJECTOR.getInstance(ConnectionHandler.class);
 
     public Server(int port) {
         Thread.currentThread().setName("Client Acceptor");
@@ -24,9 +22,8 @@ public class Server {
             int i = 0;
             while(true) {
                 try {
-                    final Socket clientSocket = serverSocket.accept();
+                    new Thread(new ClientHandlerTask(serverSocket.accept())).start();
                     logger.info("{} client connected.", ++i);
-                    connectionHandler.registerClient(clientSocket);
                 } catch (IOException e) {
                     logger.error("Server error occurred:", e);
                 }
