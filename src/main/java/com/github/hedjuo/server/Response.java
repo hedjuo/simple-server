af51b177e50237fc684c8c691c6487512b17377e
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-12-03 20:56:29 +0700

    Add errorMessage to Response.

--- src/main/java/com/github/hedjuo/server/Response.java
+++ src/main/java/com/github/hedjuo/server/Response.java
@@ -1,23 +1,23 @@
 package com.github.hedjuo.server;
 
 import java.io.Serializable;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Response implements Serializable {
     private final String sessionId;
     private final int requestId;
     private final Status status;
     private final Object result;
+    private final String errorMessage;
 
     public enum Status {
         SUCCESS, ERROR, SERVICE_NOT_FOUND, ACTION_NOT_FOUND, CONNECTION_CLOSED
     }
 
-    public Response(final String sessionId, final int requestId, final Status status, final Object result) {
+    public Response(final String sessionId, final int requestId, final Status status, final String errorMessage, final Object result) {
         this.sessionId = sessionId;
         this.requestId = requestId;
         this.status = status;
+        this.errorMessage = errorMessage;
         this.result = result;
     }
 
@@ -37,12 +37,17 @@ public class Response implements Serializable {
         return result;
     }
 
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
     @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
-        sb.append("Response { sessionId = [ "+sessionId);
-        sb.append(" ], requestId = [ "+requestId);
-        sb.append(" ], status = [ "+status);
+        sb.append("Response { sessionId = [ " + sessionId);
+        sb.append(" ], requestId = [ " + requestId);
+        sb.append(" ], status = [ " + status);
+        sb.append(" ], error = [ " + errorMessage);
         sb.append(" ], result = [ " + (result != null ? result.toString(): "null"));
         sb.append(" ]}");
         return sb.toString();
