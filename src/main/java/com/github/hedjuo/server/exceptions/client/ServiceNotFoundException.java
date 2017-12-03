5b3c7b9b16a1afc8907649e56de02750feb0d2e3
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-12-03 20:54:49 +0700

    Move ServiceNotFoundException to client package.

--- src/main/java/com/github/hedjuo/server/exceptions/client/ServiceNotFoundException.java
+++ src/main/java/com/github/hedjuo/server/exceptions/client/ServiceNotFoundException.java
@@ -1,5 +1,5 @@
-package com.github.hedjuo.server.exceptions;
+package com.github.hedjuo.server.exceptions.client;
 
-public class ServiceNotFoundException extends RuntimeException {
+public class ServiceNotFoundException extends ServiceException {
     public ServiceNotFoundException(String serviceName) { super(String.format("%s service not found.", serviceName)); }
 }
