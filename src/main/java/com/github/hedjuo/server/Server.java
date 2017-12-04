abb0a40536da7bdb4df1d7dc7f8f2d2cdd96e68b
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-12-04 11:01:09 +0700

    Add max client count restriction.

--- src/main/java/com/github/hedjuo/server/Server.java
+++ src/main/java/com/github/hedjuo/server/Server.java
@@ -8,22 +8,44 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
     public static Injector INJECTOR = Guice.createInjector(new ServerModule());
 
+    private final ExecutorService clientsPool;
 
-    public Server(int port) {
+    public Server(int port, int maxClientCount) {
         Thread.currentThread().setName("Client Acceptor");
 
+        clientsPool = new ThreadPoolExecutor(
+                maxClientCount/2,
+                maxClientCount,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>());
+
         try(final ServerSocket serverSocket = new ServerSocket(port)){
+            logger.info("Max connected client value {}", maxClientCount);
             logger.info("Server started. Listening port {}", port);
-            int i = 0;
+            final AtomicInteger clientCount = new AtomicInteger(0);
             while(true) {
                 try {
-                    new Thread(new ClientHandlerTask(serverSocket.accept())).start();
-                    logger.info("{} client connected.", ++i);
+                    final Socket clientSocket = serverSocket.accept();
+                    clientCount.incrementAndGet();
+                    if (clientCount.get() > maxClientCount) {
+                        clientCount.decrementAndGet();
+                        logger.error("Client count limit reached.");
+                        clientSocket.close();
+                        continue;
+                    }
+                    final CompletableFuture<Void> future = CompletableFuture.runAsync(new ClientHandlerTask(clientSocket), clientsPool);
+                    future.thenAccept((result) -> clientCount.decrementAndGet());
+
+                    logger.info("Client-{} connected.", clientCount.get());
                 } catch (IOException e) {
                     logger.error("Server error occurred:", e);
                 }
