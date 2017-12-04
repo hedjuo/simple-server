d8ffefdd86880f02cd1e0a47b2f26675cf10fc3c
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-12-04 11:10:31 +0700

    Pass maxClientCount parameter in ServerRunner

--- src/main/java/com/github/hedjuo/server/ServerRunner.java
+++ src/main/java/com/github/hedjuo/server/ServerRunner.java
@@ -6,11 +6,11 @@ import org.slf4j.LoggerFactory;
 public class ServerRunner {
     private static Logger logger = LoggerFactory.getLogger(ServerRunner.class);
 
-    public static void run(int portNumber) {
+    public static void run(int portNumber, int maxClientCount) {
 
         try {
             logger.info("Initializing server.");
-            new Server(portNumber);
+            new Server(portNumber, maxClientCount);
         } catch (Exception e) {
             logger.error("Unexpected error: "+ e.getMessage());
         }
