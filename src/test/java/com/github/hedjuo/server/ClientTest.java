318d3ea9159caf42beeb563b2e18951849d1bd17
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-12-08 09:37:57 +0700

    Add testUnknownService and testUnknownAction tests.

--- src/test/java/com/github/hedjuo/server/ClientTest.java
+++ src/test/java/com/github/hedjuo/server/ClientTest.java
@@ -1,13 +1,14 @@
 package com.github.hedjuo.server;
 
 import com.github.hedjuo.client.Client;
+import com.github.hedjuo.client.exceptions.ActionNotFoundException;
+import com.github.hedjuo.client.exceptions.ServiceNotFoundException;
 import com.github.hedjuo.common.Response;
 import com.github.hedjuo.common.Response.Status;
 import com.github.hedjuo.server.dto.User;
 import com.github.hedjuo.client.exceptions.ServiceException;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -16,14 +17,10 @@ import java.util.Date;
 public class ClientTest {
 
     int port = 4444;
-    @BeforeMethod
+    @BeforeClass
     public void setUp() throws Exception {
         new Thread(() -> new Server(port, 15)).start();
         Thread.currentThread().sleep(2000);
-    }
-
-    @AfterMethod
-    public void tearDown() throws Exception {
     }
 
     @Test
@@ -48,6 +45,30 @@ public class ClientTest {
             Assert.assertTrue(date != null);
         } catch (IOException | ServiceException e) {
             Assert.fail();
+        }
+    }
+
+    @Test
+    public void testUnknownService() throws InterruptedException {
+        try {
+            Client c = new Client("localhost", port, 0);
+            c.remoteCall("unknown-service", "now", new Object[]{});
+        } catch (IOException | ServiceException e) {
+            if (!(e instanceof ServiceNotFoundException)) {
+                Assert.fail();
+            }
+        }
+    }
+
+    @Test
+    public void testUnknownAction() throws InterruptedException {
+        try {
+            Client c = new Client("localhost", port, 0);
+            c.remoteCall("date-service", "unknown-action", new Object[]{});
+        } catch (IOException | ServiceException e) {
+            if (!(e instanceof ActionNotFoundException)) {
+                Assert.fail();
+            }
         }
     }
 }
